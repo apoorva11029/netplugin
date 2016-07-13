38b376bdefbc98851d412cf56ce0d4099a633015
abhinandanpb <aprativadi@gmail.com>
2016-07-13 14:24:50 -0700

    kubernetes test framework

--- netmaster/objApi/objapi_test.go
+++ netmaster/objApi/objapi_test.go
@@ -1,11 +1,9 @@
 /***
 Copyright 2014 Cisco Systems Inc. All rights reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -995,13 +993,6 @@ func TestEpgPolicies(t *testing.T) {
 	// verify network association cant be changed on epg
 	checkCreateNetwork(t, false, "default", "newnet", "data", "vxlan", "20.1.1.1/16", "20.1.1.254", 2, "", "")
 	checkCreateEpg(t, true, "default", "newnet", "group1", []string{}, []string{})
-
-	// verify name clash between network and epg is rejected
-	checkCreateEpg(t, true, "default", "contiv", "contiv", []string{})
-	checkCreateNetwork(t, true, "default", "group1", "data", "vxlan", "20.1.1.1/16", "20.1.1.254", 1)
-	// verify network association cant be changed on epg
-	checkCreateNetwork(t, false, "default", "newnet", "data", "vxlan", "20.1.1.1/16", "20.1.1.254", 2)
-	checkCreateEpg(t, true, "default", "newnet", "group1", []string{})
 
 	// change policy and verify EPG policy changes
 	checkCreateEpg(t, false, "default", "contiv", "group3", []string{"policy1"}, []string{})
