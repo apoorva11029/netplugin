b92ee71508016ef28eba73c9b9ccf5e1e1efce02
abhinandanpb <aprativadi@gmail.com>
2016-07-14 18:22:24 -0700

    kubernetes test framework

--- vagrant/k8s/setup_cluster.sh
+++ vagrant/k8s/setup_cluster.sh
@@ -87,5 +87,5 @@ vagrant up
 
 if [ "$k8_sanity" == "" ]; then
 # run ansible
-ansible-playbook -i .contiv_k8s_inventory ./../../../contrib/ansible/cluster.yml --skip-tags "contiv_restart,ovs_install,addons" -e "networking=contiv contiv_fabric_mode=default localBuildOutput=$top_dir/k8s-$k8sVer/kubernetes/server/bin contiv_bin_path=$top_dir/contiv_bin"
+ansible-playbook -i .contiv_k8s_inventory ./contrib/ansible/cluster.yml --skip-tags "contiv_restart,addons" -e "networking=contiv contiv_fabric_mode=default localBuildOutput=$top_dir/k8s-$k8sVer/kubernetes/server/bin contiv_bin_path=$top_dir/contiv_bin"
 fi
