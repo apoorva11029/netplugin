cf4c0f6e21343d22f792c7379b672fd6ca3ab7f1
apoorva11029 <apoorva11029@iiitd.ac.in>
2016-07-24 20:05:18 -0700

    baremetal middle

--- test/systemtests/init_test.go
+++ test/systemtests/init_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/contiv/contivmodel/client"
 	"github.com/contiv/remotessh"
 	. "gopkg.in/check.v1"
+	"strings"
 )
 
 type systemtestSuite struct {
@@ -87,7 +88,7 @@ func TestMain(m *M) {
 	if mastbasic.Platform == "baremetal" {
 		if mastbasic.Scheduler == "swarm" {
 			logrus.Infof("Setting up swarm cluster")
-			sts.NetDemoInstallation()
+			//sts.NetDemoInstallation()
 		}
 	}
 	flag.Parse()
@@ -119,7 +120,22 @@ func (s *systemtestSuite) SetUpSuite(c *C) {
 
 func (s *systemtestSuite) SetUpTest(c *C) {
 	logrus.Infof("============================= %s starting ==========================", c.TestName())
+	outChan := make(chan string, 100)
+        out, _ := s.nodes[0].runCommand("echo $DOCKER_HOST")
+        outChan <- out
+        logrus.Infof("DOCKER HOST for first node ====== %s", strings.TrimSpace(<-outChan))
 
+        out, _ = s.nodes[1].runCommand("echo $DOCKER_HOST")
+        outChan <- out
+        logrus.Infof("echo DOCER HOST for first node ====== %s", strings.TrimSpace(<-outChan))
+	
+	out, _ = s.nodes[0].runCommand("docker info")
+        outChan <- out
+        logrus.Infof("DOCKER info for first node ====== %s", strings.TrimSpace(<-outChan))
+
+        out, _ = s.nodes[1].runCommand("docker info")
+        outChan <- out
+        logrus.Infof("docker ifno for first node ====== %s", strings.TrimSpace(<-outChan))
 	switch s.basicInfo.Platform {
 	case "baremetal":
 		s.SetUpTestBaremetal(c)
