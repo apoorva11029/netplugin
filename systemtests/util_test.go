6ad004a65d8fd1e7fa777987b2637cd1911d9464
apoorva11029 <apoorva11029@iiitd.ac.in>
2016-08-09 10:11:17 -0700

    default nodes 2

--- systemtests/util_test.go
+++ systemtests/util_test.go
@@ -948,7 +948,7 @@ func (s *systemtestSuite) SetUpSuiteVagrant(c *C) {
 	var contivNodes int
 
 	if nodesStr == "" {
-		contivNodes = 2
+		contivNodes = 3
 	} else {
 		var err error
 		contivNodes, err = strconv.Atoi(nodesStr)
